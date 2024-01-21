9d376ce152f942659fe0db25e09c2bd7b506509f
Radko Mishev <rm.mishev@gmail.com>
2024-01-21 13:42:03 +0200

    (feat): Add ContactPage content

--- src/app-components/ContactPage/ContactPage.tsx
+++ src/app-components/ContactPage/ContactPage.tsx
@@ -1,3 +1,27 @@
 export const ContactPage = () => {
-  return <>Contacts</>;
+  return (
+    <>
+      <p id="name">Cornelius Oswald Fudge,</p>
+      <p id="ministry">Ministry of Magic Headquarters,</p>
+      <p id="location">
+        Underground of Whitehall and the HM Treasury building,
+      </p>
+      <p id="city">London,</p>
+      <p id="country">Great Britain</p>
+      {/* Picture tag goes over the children and renders the first element it
+      can. Webp images are smaller and faster to load than jpgs, however some
+      browsers (IE, Safari) do not support webp images. The picture tag allows
+      us to render a jpg if the browser does not support webp. */}
+      <picture>
+        <source
+          srcSet="/src/app-assets/webp/Corneliushogwarts.webp"
+          type="image/webp"
+        />
+        <img
+          src="/src/app-assets/jpg/Corneliushogwarts.jpg"
+          alt="Cornelius Fudge"
+        />
+      </picture>
+    </>
+  );
 };
